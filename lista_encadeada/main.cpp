c44d1d29572f1f21292c7b86a9170ec6d56ea414
Wellington Gustavo Macedo <wellingtongustavomacedo@gmail.com>
2017-07-24 14:12:20 -0300

    Verificação de memória utilizada incluída. Projeto, a princípio, finalizado

--- lista_encadeada/main.cpp
+++ lista_encadeada/main.cpp
@@ -1,28 +1,29 @@
-#include<iostream>
 #include <stdlib.h>
-#include <string>
 #include <locale.h>
 #include "GerenciadorSistema.h"
+#include <crtdbg.h>
 
 using namespace std;
 
+_CrtMemState s1;
 void exibirMemoriaUsada();
 
 int main()
-{     
+{        
    setlocale(LC_ALL, "Portuguese");
    exibirMemoriaUsada();
 
    GerenciadorSistema * gerenciador = new GerenciadorSistema();
    gerenciador->iniciarPrograma();
    delete gerenciador;
+
    exibirMemoriaUsada();
    return 0;
 }
 
-void exibirMemoriaUsada(){
-  system("tasklist /fi \"IMAGENAME eq lista_encadeada.exe\"");
-  cout << endl;
+void exibirMemoriaUsada(){ 
+   _CrtMemCheckpoint(&s1);
+   _CrtMemDumpStatistics( &s1);
 }
 
 
